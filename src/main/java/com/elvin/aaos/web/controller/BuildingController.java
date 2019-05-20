afa22b22be7d1c3c34efc04449147d0753fb16ad
Elvin Shrestha <elwyncrestha@gmail.com>
2019-05-20 10:03:24 +0545

    Add: Building Count

--- src/main/java/com/elvin/aaos/web/controller/BuildingController.java
+++ src/main/java/com/elvin/aaos/web/controller/BuildingController.java
@@ -1,9 +1,13 @@
 package com.elvin.aaos.web.controller;
 
+import com.elvin.aaos.core.service.BuildingService;
+import com.elvin.aaos.web.utility.StringConstants;
 import com.elvin.aaos.web.utility.auth.AuthenticationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,11 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/building")
 public class BuildingController {
 
+    @Autowired
+    private BuildingService buildingService;
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private void buildingPageCount(ModelMap modelMap) {
+        modelMap.put(StringConstants.BUILDING_COUNT, buildingService.countAll());
+    }
+
     @GetMapping(value = "/add")
-    public String getAddPage() {
+    public String getAddPage(ModelMap modelMap) {
         if (AuthenticationUtil.isAdmin()) {
+            buildingPageCount(modelMap);
             return "building/add";
         } else {
             return "403";

d84d5ca6cb9c76931433263393a0198a19853882
Elvin Shrestha <elwyncrestha@gmail.com>
2019-06-08 18:59:44 +0545

    Configure Mail

--- src/main/java/com/elvin/aaos/web/controller/HomeController.java
+++ src/main/java/com/elvin/aaos/web/controller/HomeController.java
@@ -1,24 +1,31 @@
 package com.elvin.aaos.web.controller;
 
+import com.elvin.aaos.mail.MailSender;
 import com.elvin.aaos.web.utility.StringConstants;
 import com.elvin.aaos.web.utility.auth.AuthenticationUtil;
 import com.elvin.aaos.web.utility.auth.Authorities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Controller
 public class HomeController {
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final MailSender mailSender;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public HomeController(
+            @Autowired MailSender mailSender
+    ) {
+        this.mailSender = mailSender;
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getHomePage() {
@@ -68,6 +75,17 @@ public class HomeController {
 
         return AuthenticationUtil.currentUserIsNull() ? "resetPassword" : "redirect:/";
 
+    }
+
+    @PostMapping(value = "/resetPassword")
+    public String resetPassword(@RequestParam("email") String email) {
+        try {
+            String message = "<h1>Hello!!!</h1>";
+            mailSender.sendMail("elwyncrestha@gmail.com", "AAOS Testing", message);
+        } catch (MessagingException e) {
+            logger.error(e.getMessage());
+        }
+        return "redirect:/login";
     }
 
 }
